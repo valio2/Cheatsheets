53d22b80f9f4b370ff6d3f9640e777499d7648c2
valio2 <Valios2@hotmail.com>
2017-11-30 10:38:26 +0200

    function factiorial formatirano po eslint

--- Function factorial.js
+++ Function factorial.js
@@ -1,14 +1,17 @@
-let n = 3;
-function factorial(n) {
+const n = 3;
+
+const factorial = (n) => {
   if (n === 0) {
     return 1;
   }
   return n * factorial(n - 1);
 }
 
-var x = factorial(19),
-	y = factorial(17),
-	z = factorial(2);
+const x = factorial(19);
+const y = factorial(17);
+const z = factorial(2);
 
-var combinations = x/(y*z);
+const combinations = x / (y * z);
 console.log(combinations);
+
+
