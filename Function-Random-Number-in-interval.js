4a7a6fbc76fb9bf1f4aaa5b7403bcbe1c3981c5b
valio2 <Valios2@hotmail.com>
2017-12-19 21:06:53 +0200

    cosmetics

---
+++ Function-Random-Number-in-interval.js
@@ -0,0 +1,1 @@
+Math.randomInt = (min, max) => Math.trunc(Math.random() * (max - min + 1)) + min;
--- Function-strings-add.js
+++ Function-strings-add.js
@@ -1,15 +1,16 @@
-const add = (a, b) => {
+const add = (a, b, c) => {
     if (+a === 0 && +b === 0) {
         return '0';
     }
 
     a = a.split('').reverse();
     b = b.split('').reverse();
+    c = c.split('').reverse();
     const result = [];
 
     for (let i = 0;
-        (a[i] >= 0) || (b[i] >= 0); i++) {
-        const sum = (parseInt(a[i]) || 0) + (parseInt(b[i]) || 0);
+        (a[i] >= 0) || (b[i] >= 0) || (c[i] >= 0); i += 1) {
+        const sum = (parseInt(a[i]) || 0) + (parseInt(b[i]) || 0) + (parseInt(c[i]) || 0);
 
         if (!result[i]) {
             result[i] = 0;
@@ -26,6 +27,8 @@ const add = (a, b) => {
     return result.reverse().join('');
 };
 
-const x = '239';
-const y = '495';
-console.log(add(x, y));
+const x = '29';
+const y = '49';
+const z = '35';
+
+console.log(add(x, y, z));
